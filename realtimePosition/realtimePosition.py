6a74ffef6bb4306aa71f553676f109d24f0e5cd0
jinhanchoi <jinhanchoi1@gmail.com>
2019-05-06 23:03:19 +0900

    json url parse example add

--- realtimePosition/realtimePosition.py
+++ realtimePosition/realtimePosition.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 #csv format
 # realtimePosition { result, doce, developerMessage, link, message, status, total}
 # [
@@ -8,16 +9,30 @@
 # ]
 # api Key : 4943656c6779736f31303174776c5845
 # url : http://swopenapi.seoul.go.kr/api/subway/4943656c6779736f31303174776c5845/xml/realtimePosition/0/23/9호선
+#http://swopenapi.seoul.go.kr/api/subway/4943656c6779736f31303174776c5845/xml/realtimePosition/0/23/9%ED%98%B8%EC%84%A0
+import urllib.request
+import urllib.parse
+import json
 
 def apiKey():
     return "4943656c6779736f31303174776c5845"
+
 def apiDefaultUri():
-    return "http://swopenapi.seoul.go.kr/api/subway/{}/xml/realtimePosition/{}/{}/{}"
+    return "http://swopenapi.seoul.go.kr/api/subway/{}/json/realtimePosition/{}/{}/{}"
+
 def getApiUrlByParam(startNum=0,endNum=30,lineName="9호선"):
-    return apiDefaultUri().format(apiKey(),startNum,endNum,lineName)
+    return apiDefaultUri().format(apiKey(),startNum,endNum,urllib.parse.quote(lineName))
+
+def apiCall(uri):
+    req = urllib.request.Request(uri)
+    res = urllib.request.urlopen(req)
+    jsonObj = json.load(res)
+    for row in jsonObj["realtimePositionList"]:
+        print(row["statnNm"])
 
 def main():
     print(getApiUrlByParam())
+    print(apiCall(getApiUrlByParam()))
 
 
 
